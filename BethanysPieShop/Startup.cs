c2df78eb75322c628b2120a40254d4469c0a681e
Vivek Sharma <thekeviv.vivek@gmail.com>
2020-05-02 06:06:34 -0700

    Added ShoppingCartController

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -29,6 +29,9 @@ namespace BethanysPieShop
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped<IPieRepository, PieRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+            services.AddHttpContextAccessor();
+            services.AddSession();
             services.AddControllersWithViews();
         }
 
@@ -47,6 +50,7 @@ namespace BethanysPieShop
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSession();
 
             app.UseRouting();
 
