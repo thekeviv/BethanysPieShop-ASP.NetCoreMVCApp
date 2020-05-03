845615a2fba90e73a2017411eebebb196c9efcca
Vivek Sharma <thekeviv.vivek@gmail.com>
2020-05-03 05:12:40 -0700

    Added Authorization on Orders Controller

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -58,7 +58,7 @@ namespace BethanysPieShop
 
             app.UseRouting();
             app.UseAuthentication();
-
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
